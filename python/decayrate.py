corrected bubble determinant formula

python/decayrate.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 from cosmoTransitions.tunneling1D import SingleFieldInstanton  # bounce
 from BubbleDet import BubbleConfig, ParticleConfig, BubbleDeterminant
 
+# Use LaTeX for all text
+plt.rcParams.update({
+    "text.usetex": False,              # don't use external LaTeX
+    "mathtext.fontset": "cm",          # use Computer Modern
+    "font.family": "serif",            # serif text
+})
+
+
 # potential and its derivatives
 
 fig1, ax1 = plt.subplots(1)
@@ -58,43 +66,51 @@ def ConstructDet(g, rho):
 
     # fluctuation determinant
     S1, S1_err = bub_det.findDeterminant()
-
-    """# printing results
-    print("S0          :", S0)
-    print("S1          :", S1)
-    print("err(S1)     :", S1_err)"""
-
+    
     return [S0, S1, S1_err]
 
-free_things = ConstructDet(0.1, 0)
+g = 2
 
-free_determinant = free_things[1]
+# calculate the free case
+free_things = ConstructDet(g, 0)
 
-def ratio(rho):
-     return (ConstructDet(0.1, rho)[1])/free_determinant
+print(free_things)
 
-def Gamma(x, B):
-    if x == 0: 
-        prefactor = 1
-    else:
-        prefactor = ratio(x)
+def Ratio(x, g, free_things):
+    #pass the vacuum case as an argument here
+    S_0 = free_things[0]
+    S_1 = free_things[1]
 
-    return (1-x)**3*prefactor*np.exp(B*(1-(1-x)**(3/2)))
+    #calculate the coupled gas actions
+    coupled_things = ConstructDet(g, x)
+    S_0_coupled = coupled_things[0]
+    S_1_coupled = coupled_things[1]
+    
+    print("Delta S_0 = " + str(S_0_coupled-S_0))
+    print("Delta S_1 = " + str(S_1_coupled-S_1))
+    print("Delta S_0  + Delta S_1 = "  + str((S_0_coupled-S_0) + (S_1_coupled-S_1)))
 
-x = np.linspace(0, 1, 200)
-B = 100
+    return (S_0_coupled/S_0)**2 * np.exp(-(S_0_coupled-S_0) - (S_1_coupled-S_1))
 
+#x is the gas density.
+x = np.linspace(0, 0.9999, 200)
 decay_rate = np.zeros(len(x))
 
 for i in range(len(x)):
-    decay_rate[i] = Gamma(x[i], B)
+    decay_rate[i] = Ratio(x[i], g, free_things)
+    print(decay_rate[i])
+    print("done: " + str(i))
 
-ax1.plot(x, decay_rate)
-ax1.set_ylabel(r"$\frac{\Gamma(\rho)}{\Gamma(0)}$", fontsize=20)
+ax1.plot(x, np.log(decay_rate), label="$\Gamma/V$: tree-level + one-loop")
+ax1.set_ylabel(r"$\ln\left(\frac{\Gamma(\rho)}{\Gamma(0)}\right)$", fontsize=20)
 ax1.set_xlabel(r"$\frac{\rho}{\rho_*}$", fontsize =20)
 ax1.set_xlim(0, 1)
+ax1.legend(loc='lower right', fontsize=15)
 ax1.grid("on")
 ax1.set_yscale("log")
 ax1.tick_params(axis='both', which='major', labelsize=15)
 ax1.tick_params(axis='both', which='minor', labelsize=15)
+plt.savefig("figure.pdf", bbox_inches="tight")
+
+plt.tight_layout()
 plt.show()